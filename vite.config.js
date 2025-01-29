df13671a61fefe4dc266a09dc87c17b32650426f
Pankaj Rajput <161428523+KumarkPankaj@users.noreply.github.com>
2025-01-29 12:45:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-scroll'],
+    },
+  },
 });
+
